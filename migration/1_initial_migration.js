83d1aae0aa41494182ba3f4f99eb82bd46e1cb84
balqisnadhrah <48413233+balqisnadhrah@users.noreply.github.com>
2020-06-06 02:54:34 +0800

    Add existing file

---
+++ migration/1_initial_migration.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// var Migrations = artifacts.require("./Migrations.sol");
+var TenancyDeposit = artifacts.require("./TenancyDeposit.sol");
+
+module.exports = function (deployer) {
+    // deployer.deploy(Migrations);
+
+    // let tenantAddress = "0xffcf8fdee72ac11b5c542428b35eef5769c409f0";
+    // let arbiterAddress = "0x22d491bde2303f2f43325b2108d26f1eaba1e32b";
+    // let expectedDeposit = 10;
+    // deployer.deploy(TenancyDeposit, tenantAddress, arbiterAddress, expectedDeposit);
+};
